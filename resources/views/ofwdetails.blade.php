a46d93024cd2f9069d1f4f1918212ba7496c21eb
Aira Mae <airamaesuazofalsario26@gmail.com>
2023-04-03 12:56:39 +0800

    dipa tapos yung box

--- resources/views/ofwdetails.blade.php
+++ resources/views/ofwdetails.blade.php
@@ -19,92 +19,61 @@
             <input type="hidden" class="form-control" id="userid" name="userid" value="{{ Auth::user()->id }}" >
           </div>
           <div class="row">
-            <div class="col-9 form-group">
-              <label for="name">Name</label>
-              <input type="text" class="form-control" id="name" name="name" value="{{ Auth::user()->name }}">
-              <small id="emailHelp" class="form-text text-muted">Last name, First name, Middle name</small>
+            <div class="col-3 form-group">
+              <label for="lastname">LASTNAME</label>
+              <input type="text" class="form-control" id="lastname" name="lastname" value="{{ Auth::user()->lastname }}">
             </div>
-            <div class="col form-group">
+            <div class="col-3 form-group">
+              <label for="firstname">FIRSTNAME</label>
+              <input type="text" class="form-control" id="firstname" name="firstname" value="{{ Auth::user()->firstname }}">
+            </div>
+            <div class="col-3 form-group">
+              <label for="middlename">MIDDLENAME</label>
+              <input type="text" class="form-control" id="middlename" name="middlename" value="{{ Auth::user()->middlename }}">
+            </div>
+            <div class="col-1 form-group">
+              <label for="suffixname">SUFFIX</label>
+              <input type="text" class="form-control" id="suffixname" name="suffixname" value="{{ Auth::user()->suffixname }}">
+            </div>
+          </div>
+          <div class ="row">
+            <div class="col-3 form-group">
+              <label for="birthday">Date of Birth</label>
+              <input type="date" class="form-control" id="birthday" name="birthday"  placeholder="">
+            </div>
+            <div class="col-2 form-group">
+              <label for="age">Age</label>
+              <input type="number" class="form-control" id="age" name="age"  placeholder="Enter Age">
+            </div>
+            <div class="col-2 form-group">
               <label for="gender">Sex</label>
               <select class="form-control" name="gender" id="gender">
                 <option value="female">Female</option>
                 <option value="male">Male</option>
               </select>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="add">Address</label>
-            <input type="text" class="form-control" id="add" name="add"  placeholder="Enter Address" >
-          </div>
-          <div class ="row">
-            <div class=" col-8 form-group">
-              <label for="emailadd">Email address</label>
-              <input type="email" class="form-control" id="emailadd" name="emailadd"  placeholder="ex. abc@gmail.com">
-            </div>
-            <div class="col form-group">
+            <div class="col-3 form-group">
               <label for="contactnum">Contact Number</label>
               <input type="number" class="form-control" id="contactnum" name="contactnum"  placeholder="ex. 09123456789">
             </div>
           </div>
           <div class="row">
-            <div class="col-10 form-group">
-              <label for="birthplace">Place of Birth</label>
-              <input type="text" class="form-control" id="birthplace" name="birthplace"  placeholder="Enter place of birth">
-            </div>
-            <div class="col form-group">
-              <label for="birthday">Date of Birth</label>
-              <input type="date" class="form-control" id="birthday" name="birthday"  placeholder="">
-            </div>
+            <div class="col-6 form-group">
+            <label for="add">Address</label>
+            <input type="text" class="form-control" id="add" name="add"  placeholder="Enter Address" >
           </div>
-          <div class="row">
-            <div class="col form-group">
-              <label for="age">Age</label>
-              <input type="number" class="form-control" id="age" name="age"  placeholder="Enter Age">
+          <div class="col-3 from group">
+              <label for ="passportnum">Passport Number</label>
+              <input type="text" class="form-control" id="passportnum" name="passportnum" value="{{ Auth::user()->name }}">
             </div>
-            <div class="col form-group">
-              <label for="height">Height</label>
-              <input type="number" class="form-control" id="height" name="height"  placeholder="Enter Height">
+          <div class ="row">
+            <div class=" col-5 form-group">
+              <label for="emailadd">Email address</label>
+              <input type="email" class="form-control" id="emailadd" name="emailadd"  placeholder="ex. abc@gmail.com">
             </div>
-            <div class="col form-group">
-              <label for="weight">Weight</label>
-              <input type="number" class="form-control" id="weight" name="weight"  placeholder="Enter Weight">
-            </div>
-            <div class="col form-group">
-              <label for="bloodtype">Bloodtype</label>
-              <select class="form-control" name="bloodtype" id="bloodtype">
-                <option value="N/A">Not Applicable</option>
-                <option value="O+">O positive</option>
-                <option value="O-">O negative</option>
-                <option value="A+">A positive</option>
-                <option value="A-">A negative</option>
-                <option value="B+">B positive</option>
-                <option value="B-">B negative</option>
-                <option value="AB+">AB positive</option>
-                <option value="AB-">AB negative</option>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="religion">Religion</label>
-            <input type="text" class="form-control" id="religion" name="religion"  placeholder="Enter Religion">
-          </div>
-          <div class="row">
-            <div class="col-8 form-group">
-              <label for="guardian">Name of Guardian</label>
-              <input type="text" class="form-control" id="guardian" name="guardian"  placeholder="Enter name of Guardian">
-              <small id="guardian" class="form-text text-muted">Last name, First name, Middle name</small>
-            </div>
-            <div class="col form-group">
-              <label for="relationship">Relationship with the Applicant</label>
-              <select class="form-control" name="relationship" id="relationship">
-                <option value="daughter">Daughter</option>
-                <option value="son">Son</option>
-                <option value="niece">Niece</option>
-                <option value="nephew">Nephew</option>
-                <option value="sister">Sister</option>
-                <option value="brother">Brother</option>
-              </select>
-            </div>          
+            <div class="col-5 form-group">
+              <label for="facebook">Facebook Account</label>
+              <input type="facebook" class="form-control" id="facebook" name="facebook">
           </div>
           <div class="row">
             <div class="col-5">&nbsp;</div>
