90e4eaaf0287b4d0268d6df9e6866de91a4a8444
kate <mryktlynln@gmail.com>
2023-05-30 12:16:30 +0800

    polishhh

--- resources/views/adminhome.blade.php
+++ resources/views/adminhome.blade.php
@@ -8,7 +8,7 @@
       <div class="container" data-aos="fade-up">
           <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
             <div class="col-xl-6 col-lg-8">
-              <h1>Provincial Peso Service Management System</h1>
+              <h1>Provincial Peso Services Management System</h1>
               <h2>Camilmil, Calapan City, Oriental Mindoro</h2>
             </div>
           </div>
@@ -19,12 +19,6 @@
               <div class="icon-box">
                 <i class="ri-calendar-todo-line"></i>
                 <h3><a href="Announcements">General</a></h3>
-              </div>
-            </div>
-            <div class="col-xl-2 col-md-4">
-              <div class="icon-box">
-                <i class="ri-database-2-line"></i>
-                <h3><a href="GeneralA">Services</a></h3>
               </div>
             </div>
           </div>
