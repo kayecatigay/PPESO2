926ff1fc626a5a133f9bdebd1bc52f75c459bc8d
kate <mryktlynln@gmail.com>
2023-09-02 21:22:02 +0800

    services

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -9,34 +9,42 @@
             <p>Check our Services</p>
           </div>
 
-            <div class="row">
-              <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                <div class="icon-box">
-                  <div class="icon"><i class="bi bi-mortarboard-fill"></i></div>
-                  <h4><a href="scholarhomepage">Scholarship</a></h4>
-                  <p>PESO Educational Assistance Program.</p>
-                  <p>Apply for more information.</p>
-                </div>
+          <div class="d-flex justify-content-around" >
+              
+              <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="100" >
+                <a href="scholarhomepage">
+                  <div class="icon-box" style="color:black;">
+                    <div class="icon"><i class="bi bi-mortarboard-fill"></i></div>
+                    <h4>Scholarship</h4>
+                    <p>PESO Educational Assistance Program.</p>
+                    <p>Apply for more information.</p>
+                  </div>
+                </a>
               </div>
+            
 
-              <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
-                <div class="icon-box">
+            <div class="col-lg-4 col-md-6 mt-4 mt-md-0" data-aos="zoom-in" data-aos-delay="200">
+              <a href="employmenthomepage">
+                <div class="icon-box" style="color:black;">
                   <div class="icon"><i class="bi bi-briefcase-fill"></i></div>
-                  <h4><a href="employmenthomepage">Employment</a></h4>
+                  <h4>Employment</h4>
                   <p>Jobs are hiring!</p>
                   <p>Apply for more information.</p>
                 </div>
-              </div>
+              </a>
+            </div>
 
-              <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
-                <div class="icon-box">
+            <div class="col-lg-4 col-md-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="300">
+              <a href="ofwhomepage">
+                <div class="icon-box" style="color:black;">
                   <div class="icon"><i class="bi bi-airplane-engines-fill"></i></div>
-                  <h4><a href="ofwhomepage">OFW</a></h4>
+                  <h4>OFW</h4>
                   <p>OFW Assistance Program</p>
                   <p>Apply for more information.</p>
                 </div>
-              </div>
+              </a>
             </div>
+          </div>
  
       </div>
     </section><!-- End Services Section -->
