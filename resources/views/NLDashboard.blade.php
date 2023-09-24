478035fcae8b9ce6044e55e5886ef7819197b0f2
kate <mryktlynln@gmail.com>
2023-09-25 04:14:55 +0800

    addashboard

--- resources/views/NLDashboard.blade.php
+++ resources/views/NLDashboard.blade.php
@@ -8,7 +8,8 @@
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
-        <div class="container-fluid" style="font-family: arial, serif;  width: 70%;">
+        <div class="container-fluid" style="font-family: arial, serif; 
+                    display: block; margin-left: auto; margin-right: auto; width: 80%;">
             @section('dashboard')
                 <!-- Page Heading -->
                 <div style="display: block; margin-left: auto; margin-right: auto; width: 80%;">
@@ -26,12 +27,13 @@
                             <h3 style="text-align:center;"> Total Users = {{$totalusers}} </h3></div>
                         </div>
                     </div>
-                <div class="row xl-4 col-lg-5 center" style=" margin: auto; margin-right: auto; width: 200%;">
+                <div class="row col-lg-5 center" style=" margin-left: auto; margin-right: auto; width: 90%;">
                     <div class="card shadow mb-3">
                         <!-- Card Header - Dropdown -->
                         <div
                             class="card-header py-7 d-flex flex-row">
-                            <h6 class="m-0 font-weight-bold text-dark" style="display: block; margin-left: auto; margin-right:suto;">ACCEPTED</h6>
+                            <h6 class="m-0 font-weight-bold text-dark" 
+                            style="text-align:center;">ACCEPTED</h6>
                             
                         </div>
                         <!-- Card Body -->
@@ -75,10 +77,8 @@
                     </div>              
                 </div>
 
-                <!-- Content Row -->
-
-                <div class="row">
-
+                <div class="container center">
+                    
                     <!-- Area Chart -->
                     <div class="col-xl-8 col-lg-7">
                         <div class="card shadow mb-4">
@@ -94,68 +94,7 @@
                             </div>
                         </div>
                     </div>
-
-                    
-
-                <!-- Content Row -->
-                <div class="row">
-
-                    <!-- Content Column -->
-                    <div class="col-lg-6 mb-4">
-
-                        <!-- Project Card Example
-                        <div class="card shadow mb-4">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Projects</h6>
-                            </div>
-                            <div class="card-body">
-                                
-                                <h4 class="small font-weight-bold">Customer Database <span
-                                        class="float-right">60%</span></h4>
-                                <div class="progress mb-4">
-                                    <div class="progress-bar" role="progressbar" style="width: 60%"
-                                        aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                                
-                                <h4 class="small font-weight-bold">Account Setup <span
-                                        class="float-right">Complete!</span></h4>
-                                <div class="progress">
-                                    <div class="progress-bar bg-success" role="progressbar" style="width: 100%"
-                                        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    </div>
-
-                    <div class="col-lg-6 mb-4"> -->
-
-                        <!-- Illustrations -->
-                        <!-- <div class="card shadow mb-4">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                            </div>
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                                        src="assets/img/features.jpg" alt="...">
-                                </div>
-                                <p>Add some quality, svg illustrations to your project courtesy of <a
-                                        target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                                    constantly updated collection of beautiful svg images that you can use
-                                    completely free and without attribution!</p>
-                                <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
-                                    unDraw &rarr;</a>
-                            </div>
-                        </div> -->
-
-                        <!-- Approach -->
-                       
-
-                    </div>
                 </div>
-
             </div>
             <!-- /.container-fluid -->
             @show
@@ -163,7 +102,7 @@
         </div>
         <!-- End of Main Content -->
     
-    
+    </div>
     <!-- End of Content Wrapper -->
 
 <script>
