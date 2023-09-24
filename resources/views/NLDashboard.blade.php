8672dfe0a3589a1309434b0ef36ec87aa50e4f2c
kate <mryktlynln@gmail.com>
2023-09-24 14:51:37 +0800

    dashboard print half done

--- resources/views/NLDashboard.blade.php
+++ resources/views/NLDashboard.blade.php
@@ -1,5 +1,6 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+<link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Main Content -->
     <div id="content">
@@ -7,90 +8,71 @@
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
-        <div class="container-fluid" style="font-family: arial, serif;">
+        <div class="container-fluid" style="font-family: arial, serif;  width: 70%;">
             @section('dashboard')
                 <!-- Page Heading -->
-                
-                <div class="d-sm-flex align-items-center justify-content-between mb-4"><BR><BR></BR></BR>
-                    <h1 style="text-align:center;" class="h3 mb-0 text-gray-800">PROVINCIAL PESO SERVICES MANAGEMENT SYSTEM</h1>
-                    <h3 style="text-align:center;" class="h3 mb-0 text-gray-800">DASHBOARD</h3>
-                   
+                <div style="display: block; margin-left: auto; margin-right: auto; width: 80%;">
+                    <BR>
+                        <h1 style="text-align:center;" class="h3 mb-0 text-gray-800">PROVINCIAL PESO SERVICES MANAGEMENT SYSTEM</h1> <BR></BR>
+                        <h3 style="text-align:center;" class="h3 mb-0 text-gray-800">DASHBOARD</h3>
+                    
                 </div>
 
                 <!-- Content Row -->
-                <div class="row">
 
                     <!-- Earnings (Monthly) Card Example -->
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-primary shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-dark text-uppercase mb-1"><br>
-                                            <h3 style="text-align:center;"> Total Users = {{$totalusers}} </h3></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="col mr-5">
+                        <div class="text-dark mb-1"><br>
+                            <h3 style="text-align:center;"> Total Users = {{$totalusers}} </h3></div>
                         </div>
-                    </div>  
-                    <div class="col-xl-4 col-lg-5">
-                        <div class="card shadow mb-3">
-                            <!-- Card Header - Dropdown -->
-                            <div
-                                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-dark">ACCEPTED</h6>
+                    </div>
+                <div class="row xl-4 col-lg-5 center" style=" margin: auto; margin-right: auto; width: 200%;">
+                    <div class="card shadow mb-3">
+                        <!-- Card Header - Dropdown -->
+                        <div
+                            class="card-header py-7 d-flex flex-row">
+                            <h6 class="m-0 font-weight-bold text-dark" style="display: block; margin-left: auto; margin-right:suto;">ACCEPTED</h6>
+                            
+                        </div>
+                        <!-- Card Body -->
+                        <div class="card-body">
+                            
+                            
                                 
-                            </div>
-                            <!-- Card Body -->
-                            <div class="card-body">
-                                
-                                <div class="mt-7 text-center small">
+                                <span class="mr-2">
                                     
-                                    <span class="mr-5">
-                                        
-                                        <i class="fa fa-graduation-cap text-primary"> {{ $apeap}} </i> PEAP
-                                    </span>
-                                    <span class="mr-5">
-                                        
-                                        <i class="fa fa-briefcase text-success"> {{ $aemp }}</i> EMP
-                                    </span>
-                                    <span class="mr-2">
-                                        
-                                        <i class="fa fa-plane text-info"> {{ $aofw }}</i> OFW
-                                    </span>
-                                </div>
-                                
-                            </div> 
+                                    <i class="text-primary font-weight-bold">{{ $apeap}} </i> PEAP
+                                </span>
+                                <span class="mr-2">
+                                    
+                                    <i class="text-success font-weight-bold">{{ $aemp }}</i> EMP
+                                </span>
+                                <span class="mr-2">
+                                    
+                                    <i class="text-info font-weight-bold">{{ $aofw }}</i> OFW
+                                </span>
+                            
+                        </div> 
+                    </div> &emsp;
+                    <div class="card shadow mb-3" >
+                        <div class="card-header py-7 d-flex ">
+                            <h6 class="m-0 font-weight-bold text-dark">COMPANY</h6><h6 class="m-0 font-weight-bold text-dark">&emsp; USER</h6>
                         </div>
-                        <div class="card shadow mb-3">
-                            <div
-                                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                <h6 class="m-0 font-weight-bold text-dark">COMPANY</h6><h6 class="m-0 font-weight-bold text-dark">APPLICANTS</h6>
-                                
-                            </div>
-                            <!-- Card Body -->
-
-                                    <div class="card-body">
-                                    @foreach ($company as $com)
-                                        <h4 
-                                            class="small font-weight-bold">{{ $com->cname }}<span
-                                            class="float-right">{{ $com->totalapp }}</span>
-                                        </h4>
-                                        <div class="progress mb-2">
-                                        <?php $percentage = ($com->totalapp / 100); ?>
-                                        <div class="progress-bar" role="progressbar" style="width: <?= $percentage ?>%;" 
-                                        aria-valuenow="<?= $com->totalapp ?>" aria-valuemin="0" aria-valuemax="100"></div>
-                                        </div>
-                                    @endforeach
+                        <!-- Card Body -->
+                            <div class="card-body">
+                                @foreach ($company as $com)
+                                    <h4 
+                                        class="small font-weight-bold">{{ $com->cname }}<span
+                                        class="float-right">{{ $com->totalapp }}</span>
+                                    </h4>
+                                    <div class="progress mb-2">
+                                    <?php $percentage = ($com->totalapp / 100); ?>
+                                    <div class="progress-bar" role="progressbar" style="width: <?= $percentage ?>%;" 
+                                    aria-valuenow="<?= $com->totalapp ?>" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
-                               
-                                
-                        </div>
-                    </div>                 
+                                @endforeach
+                            </div>
+                    </div>              
                 </div>
 
                 <!-- Content Row -->
