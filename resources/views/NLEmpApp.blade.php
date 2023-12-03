9f1157e4b643b5c94acbc02643dde4daa506f833
Mary Katelyn <mryktlynln@gmail.com>
2023-12-04 01:29:16 +0800

    polishing14

--- resources/views/NLEmpApp.blade.php
+++ resources/views/NLEmpApp.blade.php
@@ -1,7 +1,6 @@
-<header id="header">
-    <img src="assets/images/header.jpg" style="width:1300px; height:150px;" alt="icon"> &nbsp; &nbsp;
-</header>
-
+<header id="header" >
+        <img src="assets/img/ofw.png" alt="icon"> &nbsp; &nbsp;
+    </header>
 <div class="col-xl-20">
     <div class="card shadow mb-4">
         <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
@@ -9,8 +8,8 @@
                 <div class="col"><br> <br> <br>
                     <h1 class="m-0 font-weight-bold text-dark" style="text-align:center;
                         font-family:Verdana, Geneva, Tahoma, sans-serif">Application</h1>
-                           <table class="table" style="text-align:center; font-family:Verdana, Geneva, Tahoma, sans-serif">
-                              <thead>
+                           <table class="table " style="text-align:center; font-family:Verdana, Geneva, Tahoma, sans-serif">
+                              <thead class="align-items-center">
                                  <tr>
                                        <th scope="col">File Name</th>
                                        <th scope="col">Original Name</th>
@@ -19,7 +18,7 @@
                                        <th scope="col">Download File</th>   
                                  </tr>
                               </thead>
-                              <tbody >
+                              <tbody class="align-items-center">
                                  @forelse ($files as $file)
                                     <tr style="text-align:center;">
                                        <td>{{ $file->filename }}</td>
--- resources/views/editW.blade.php
+++ resources/views/editW.blade.php
@@ -35,45 +35,6 @@
                </div>
             </div> <br>
             <div class="row">
-               <div class="col-4 form-group">
-                  <label for="skills">Skills</label>
-                  <input type="hidden" size="30" id="skills" name="skills" value="{{$wrk[0]->skills}}">
-                  <div class="col">
-                     <input type="checkbox" id="hardworking" name="hardworking">
-                     <label for="hardworking">Hardworking</label> <br>
-                     <input type="checkbox" id="risk" name="risk">
-                     <label for="risk">Risk taker</label> <br>
-                     <input type="checkbox" id="probsol" name="probsol">
-                     <label for="probsol">Problem Solving</label> <br>
-                     <input type="checkbox" id="creative" name="creative">
-                     <label for="creative">Creative</label> <br>
-                     <input type="checkbox" id="multitask" name="multitask">
-                     <label for="multitask">Multitasking</label> <br>
-                     <input type="checkbox" id="technical" name="technical">
-                     <label for="technical">Technicality</label> <br>
-                     <input type="checkbox" id="leadership" name="leadership">
-                     <label for="leadership">Leadership Skills</label> <br>
-                     <input type="checkbox" id="analytics" name="analytics">
-                     <label for="analytics">Analytical Skills</label> <br>
-                  </div>
-               </div>
-               <div class="col form-group">
-                  <label for="req">Requirements</label>
-                  <input type="hidden" size="30" id="req" name="req" value="{{$wrk[0]->req}}">
-                  <div class="col">
-                     <input type="checkbox" id="resume" name="resume">
-                     <label for="resume">Resume</label> <br>
-                     <input type="checkbox" id="visa" name="visa">
-                     <label for="visa">Visa</label> <br>
-                     <input type="checkbox" id="indigency" name="indigency">
-                     <label for="indigency">Certificate of Indigency</label><br>
-                     <input type="checkbox" id="psa" name="psa">
-                     <label for="psa">PSA</label> <br>
-                     
-                  </div>
-               </div>
-            </div>
-            <div class="row">
                <div class="col-5">&nbsp;</div>
                <div class="col">
                   <button type="submit" style="background-color:#5F9EA0; border:none; border-radius: 4px;" value="Submit">Submit</button>
