7c0f926ded8ab7f2518944cb73c1e87cee6bf83a
Mary Katelyn <mryktlynln@gmail.com>
2023-12-21 10:46:28 +0800

    polishing 23

--- resources/views/employerW.blade.php
+++ resources/views/employerW.blade.php
@@ -12,7 +12,7 @@
          <div class="card shadow mb-4">
             <div
                class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-               <h6 class="m-0 font-weight-bold text-dark">Employer</h6>
+               <h6 class="m-0 font-weight-bold text-dark">Accepted Employer</h6>
                <a href="/EmpPrint" target="_blank" style="padding:1px 10px;" class="btn btn-outline-dark">Print</a>
             </div>
                
